7a1c7a74f812ddf455531e250e1a0b2073ac4f27
ClaireMilk <glittlewest@gmail.com>
2023-05-07 23:07:23 +0100

    1

--- Assets/Scripts/Airplane.cs
+++ Assets/Scripts/Airplane.cs
@@ -7,11 +7,25 @@ namespace Phantom
     public class Airplane : MonoBehaviour
     {
         public GameObject flyAnim;
+        public GameObject hintUI;
+
         public void AirplaneFly(int flyEvent)
         {
             if(flyEvent == 0)
             {
                 flyAnim.SetActive(true);
+            }
+            if(flyEvent == -1)
+            {
+                hintUI.SetActive(false);
+            }
+        }
+
+        public void PlaneDisappear(int flyEvent)
+        {
+            if (flyEvent == 1)
+            {
+                flyAnim.SetActive(false);
             }
         }
     }
