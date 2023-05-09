84d62a18b99ae5db69239a06cf29a1147763ae43
ClaireMilk <glittlewest@gmail.com>
2023-05-09 04:04:25 +0100

    1

---
+++ Assets/Scripts/BadEndingAnim.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BadEndingAnim : MonoBehaviour
+{
+    public float endTime;
+    public GameObject arm;
+    public GameObject pistol;
+    public Text usePistol;
+    public AudioSource footstep;
+    public AudioSource gunShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("BreakArm", endTime);
+        Invoke("KeyUI", endTime + 3.0f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            usePistol.enabled = false;
+        }
+    }
+
+    public void Fall()
+    {
+        footstep.Stop();
+    }
+
+    public void BreakArm()
+    {
+        footstep.Play();
+        arm.SetActive(true);
+        pistol.SetActive(true);
+    }
+
+    void KeyUI()
+    {
+        usePistol.enabled = true;
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene(5);
+    }
+}
+
